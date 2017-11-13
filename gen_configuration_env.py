8750a934e21f086d3a9927a1a9bcced829614461
Tom Wallroth <tomwallroth@gmail.com>
2017-11-13 11:14:29 +0000

    fixed gen_configuration_env.py

--- gen_configuration_env.py
+++ gen_configuration_env.py
@@ -1,7 +1,7 @@
 import random
 import string
 alphabet = "{}{}{}".format(string.ascii_letters, string.digits, string.punctuation)
-django_secret = "".join([random.SystemRandom().choice(alphabet) for i in range(50)]
+django_secret = "".join([random.SystemRandom().choice(alphabet) for i in range(50)])
 print('DJANGO_SECRET_KEY={}'.format(django_secret))
 print('# ALLOWED_HOSTS is a comma separated list of allowed hosts')
 print('ALLOWED_HOSTS=localhost')
