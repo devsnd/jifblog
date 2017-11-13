fcbe7082cca1425f0f1146907e1fc1634c2bc626
Tom Wallroth <tomwallroth@gmail.com>
2017-11-13 11:42:21 +0000

    fixed minimum height for jif frames, only serve media and static from django in debug mode

--- jifblog/urls.py
+++ jifblog/urls.py
@@ -20,8 +20,9 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
-urlpatterns += static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static.static(settings.STATIC_URL, document_root=settings.STATIC_URL)
+if settings.DEBUG:
+    urlpatterns += static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static.static(settings.STATIC_URL, document_root=settings.STATIC_URL)
 urlpatterns += [
     url(r'^', include('feed.urls', namespace='feed')),
 ]
